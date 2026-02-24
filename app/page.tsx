dfe09fccbbba072205b502a607933fbfffa528cc
mukhut <mukhutjp@gmail.com>
2026-02-24 15:40:10 +0100

    MO

--- app/page.tsx
+++ app/page.tsx
@@ -37,10 +37,7 @@ export default function Home() {
                   Se connecter
                 </Link>
               )}
-              <Link href={"/sign-up"}
-                className="btn btn-sm md:btn-md ml-2 btn-accent">
-                S'inscrire
-              </Link>
+              
             </div>
 
           </div>
